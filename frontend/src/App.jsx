a89a3b110a3cc84bba638c6c24dfced0f92e5ba4
ericwillyams <kw02191@gmail.com>
2023-09-15 20:07:27 -0400

    newhomebranch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,15 @@ import DeleteUser from './components/DeleteUser';
 import EditUser from './components/EditUser';
 import UserProfile from './components/UserProfile';
 import { MainPage } from './components/home';
-import { Puzzle } from './components/PuzzlePage';
+import { PuzzleComponent } from './components/PuzzlePage';
 // import withAuth from './withAuth';
 import { Link } from 'react-router-dom'; // this link and the nav component for logout will need to be moved to be moved to the navbar upon merge
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from '../components/Nav';
-import { Scoreboard } from '../components/ScoreBoard';
-import { Theme } from '../components/PuzzleTheme';
-// import { Theme } from './components/theme'
+// import { Navbar } from '../components/Nav';
+import { Scoreboard } from './components/ScoreBoard';
+import { Theme } from './components/PuzzleTheme';
 import "./App.css";
-
+import DifficultySelector from './components/DifficultySelector';
 // import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
 // import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
 // import img from "./gfgg.png";
@@ -41,14 +40,16 @@ function App() {
         <Routes>
            <Route path='/register' element={<CreateUser />} />
            <Route path='/login' element={<UserLogin />} />
+           <Route path='/logout' element={<UserLogout />} />
            <Route path='/home' element={<MainPage />} />
-           <Route path='/puzzle' element={<Puzzle />} />
-           <Route path='/scoreboard' element={<Scoreboard />} />
-           <Route path='/theme' element={<Theme />} />
+           <Route path='/home/puzzle' element={<PuzzleComponent />} />
+           <Route path='/home/scoreboard' element={<Scoreboard />} />
+           <Route path='/home/theme' element={<Theme />} />
+           <Route path='/home/theme/difficultyselector' element={<DifficultySelector />} />
            <Route path='/profile/:userId' element={<UserProfile />} />
            <Route path='/edit/:userId' element={<EditUser />} />
            <Route path='/delete/:userId' element={<DeleteUser />} />
-           <Route path='/useApiFetcherThemeImages' element={<PixabayImageFetcher />} />
+           {/* <Route path='/useApiFetcherThemeImages' element={<PixabayImageFetcher />} /> */}
 
         </Routes>
       </Router>
--- frontend/src/components/DeleteUser.jsx
+++ frontend/src/components/DeleteUser.jsx
@@ -1,12 +1,90 @@
+// import React, { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// function DeleteUser() {
+//   const [error, setError] = useState(null);
+//   const [isDeleting, setIsDeleting] = useState(false);
+//   const { userId } = useParams();
+//   useEffect(() => {
+//     // Create a variable to store whether the component is mounted
+//     let isMounted = true;
+//     // Function to handle the user deletion
+//     const deleteUser = async () => {
+//       try {
+//         // Send a DELETE request to delete the user
+//         const response = await fetch(`http://localhost:3002/users/delete/${userId}`, {
+//           method: 'DELETE',
+//         });
+//         if (response.ok && isMounted) {
+//         if (response.ok && isMounted) {
+//           // Handle successful deletion (e.g., display a success message)
+//           console.log('User deleted successfully');
+//         } else if (isMounted) {
+//         } else if (isMounted) {
+//           // Handle deletion error (e.g., display an error message)
+//           const errorData = await response.json();
+//           setError(errorData.error || 'User deletion failed');
+//         }
+//       } catch (error) {
+//         // Handle network errors or exceptions
+//         if (isMounted) {
+//           console.error('User deletion error:', error.message);
+//           setError('Network error, please try again');
+//         }
+//         if (isMounted) {
+//           console.error('User deletion error:', error.message);
+//           setError('Network error, please try again');
+//         }
+//       } finally {
+//         // If the component is mounted, update the state
+//         if (isMounted) {
+//           setIsDeleting(false);
+//         }
+//       }
+//         // If the component is mounted, update the state
+//         if (isMounted) {
+//           setIsDeleting(false);
+//         }
+//       };
+    
+//     setIsDeleting(true);
+//     deleteUser();
+//     return () => {
+//       isMounted = false;
+//     };
+//     return () => {
+//       isMounted = false;
+//     };
+//   }, [userId];
+//   return (
+//     <div>
+//       <h2>Delete User</h2>
+//       {isDeleting ? (
+//         <p>Deleting user...</p>
+//       ) : (
+//         <div>
+//           {error ? (
+//             <div className="error-message">{error}</div>
+//           ) : (
+//             <p>User deleted successfully</p>
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+// export default DeleteUser;
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
 function DeleteUser() {
   const [error, setError] = useState(null);
   const [isDeleting, setIsDeleting] = useState(false);
   const { userId } = useParams();
+
   useEffect(() => {
     // Create a variable to store whether the component is mounted
     let isMounted = true;
+
     // Function to handle the user deletion
     const deleteUser = async () => {
       try {
@@ -14,11 +92,10 @@ function DeleteUser() {
         const response = await fetch(`http://localhost:3002/users/delete/${userId}`, {
           method: 'DELETE',
         });
-        if (response.ok && isMounted) {
+
         if (response.ok && isMounted) {
           // Handle successful deletion (e.g., display a success message)
           console.log('User deleted successfully');
-        } else if (isMounted) {
         } else if (isMounted) {
           // Handle deletion error (e.g., display an error message)
           const errorData = await response.json();
@@ -30,31 +107,22 @@ function DeleteUser() {
           console.error('User deletion error:', error.message);
           setError('Network error, please try again');
         }
-        if (isMounted) {
-          console.error('User deletion error:', error.message);
-          setError('Network error, please try again');
-        }
       } finally {
         // If the component is mounted, update the state
         if (isMounted) {
           setIsDeleting(false);
         }
       }
-        // If the component is mounted, update the state
-        if (isMounted) {
-          setIsDeleting(false);
-        }
-      }
     };
+
     setIsDeleting(true);
     deleteUser();
-    return () => {
-      isMounted = false;
-    };
+
     return () => {
       isMounted = false;
     };
   }, [userId]);
+
   return (
     <div>
       <h2>Delete User</h2>
@@ -72,7 +140,9 @@ function DeleteUser() {
     </div>
   );
 }
+
 export default DeleteUser;
+
 
 
 
