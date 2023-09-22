545f1adfb32fc6edcac48185864efd4e8af103cb
LaDonte LeBlanc <donte.11@hotmail.com>
2023-09-21 20:38:24 -0400

    changed nav name for deploy

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,80 @@
+// import React from "react";
+// import { Link, } from "react-router-dom";
+// import { User } from 'phosphor-react'
+// import './navbar.css'
+// export const Navbar = () => {
+//   return (
+//     <>
+//       <div id="navBar" className="navbar fixed-top navbar-light bg-light">  
+//       <ul className="nav nav-pills nav-fill"> 
+//           <li className="nav-item">
+//             <a className="nav-link" href="/home">
+//               Home
+//             </a>
+//           </li>
+//           <li className="nav-item">
+//             <a className="nav-link" href="/home/theme">
+//               Theme
+//             </a>
+//           </li>
+//           <li className="nav-item">
+//             <a className="nav-link" href="/home/puzzle">
+//               Puzzle
+//             </a>
+//           </li>
+//           <li className="nav-item">
+//             <a className="nav-link" href="/home/scoreboard">  
+//               Scoreboard
+//             </a> 
+//           </li> 
+//         </ul>  <Link to='/profile/userId'>My Profile</Link><Link to='/logout'>Logout</Link> <User size={32}  />
+//       </div> 
+
+//     </>
+//   );
+// };
+
+import React from "react";
+import { Link, NavLink } from "react-router-dom"; // Import NavLink for active link styling
+import { User } from 'phosphor-react'
+import './navbar.css'
+
+export const Navbar = () => {
+  return (
+    <>
+      <div id="navBar" className="navbar fixed-top navbar-light bg-light">  
+        <ul className="nav nav-pills nav-fill"> 
+          <li className="nav-item">
+            <a className="nav-link" href="/welcome">
+              Start
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/home/theme">
+              Play
+            </a>
+          </li>
+
+          <li className="nav-item">
+            <NavLink to="/home/scoreboard" className="nav-link">
+              Scoreboard
+            </NavLink>
+          </li> 
+        </ul>
+        <Link to='/profile/userId'>My Profile</Link>
+        <Link to='/logout'>Logout</Link>
+        <User size={32}  />
+      </div> 
+    </>
+  );
+};
+
+
+
+
+
+
+
+
+
+
