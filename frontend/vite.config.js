16020d2d7493fdebb81afa92a30d935d1befb437
LaDonte LeBlanc <donte.11@hotmail.com>
2023-09-23 16:33:08 -0400

    update backend node for deploy-3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,28 +1,28 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()]
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
+})
 
-// vite.config.js
-import react from '@vitejs/plugin-react';
+vite.config.js
+// import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [react()],
-  server: {
-    port: 5173, // Customize the development server port
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Create custom aliases for your project's directories
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      // Add any CSS preprocessor options here
-    },
-  },
-};
+// export default {
+//   plugins: [react()],
+//   server: {
+//     port: 5173, // Customize the development server port
+//   },
+//   resolve: {
+//     alias: {
+//       '@': '/src', // Create custom aliases for your project's directories
+//     },
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       // Add any CSS preprocessor options here
+//     },
+//   },
+// };
 
